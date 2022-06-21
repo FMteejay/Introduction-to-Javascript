7ab1312c4db9754683dc5ed53895beee79fdab0d
Oluwafemi Aderemi <105191094+FMteejay@users.noreply.github.com>
2022-06-21 02:28:43 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var myName = window.prompt("What's your name?");
+
+console.log("Hello",myName);
+
+var myHeight =window.prompt("what's your height");
+
+console.log("Height",myHeight);
+
+var myCountry = window.prompt("What's your country");
+
+console.log("Country", myCountry);
